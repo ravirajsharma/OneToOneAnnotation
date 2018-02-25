b2b1f494a4b99241373755d416702de74bacf4e3
Raviraj <ravirajsharma1511@gmail.com>
2018-02-25 13:39:29 +0530

    ok

---
+++ src/main/java/com/scp/hibernatedemo/OneToOneAnnotation/HibernateStandAlone.java
@@ -0,0 +1,10 @@
+package com.scp.hibernatedemo.OneToOneAnnotation;
+
+public class HibernateStandAlone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
